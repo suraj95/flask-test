8a28e0a107b5b8d7223d166e317288c3ed38a215
Suraj Patil <srpatil@uci.edu>
2020-05-22 13:48:13 +0530

    Use a single template for all three pages

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-app = Flask(__name__, template_folder='/Users/srpatil/Desktop/Suraj/DevOps/Projects/flask-test')
+app = Flask(__name__, 
+	template_folder='/Users/srpatil/Desktop/Suraj/DevOps/Projects/flask-test/templates',
+	static_folder='/Users/srpatil/Desktop/Suraj/DevOps/Projects/flask-test/static')
 
 connection = mysql.connector.connect(
 	user = 'root',
@@ -11,23 +13,31 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 
 @app.route('/')
-def index():
+@app.route('/home')
+def home():
 	cursor.execute("SELECT * from `Inventory`.`tbl_product_movement`;")
 	data = cursor.fetchall() #data from database
-	return render_template("index.html", value=data)
+	return render_template("template.html", my_string="Product Movement Table", value=data)
 
 @app.route('/products')
 def products():
 	cursor.execute("SELECT * from `Inventory`.`tbl_product`;")
 	data = cursor.fetchall() #data from database
-	return render_template("products.html", value=data)
-
+	return render_template("template.html", my_string="Product Table", value=data)
 
 @app.route('/locations')
 def locations():
 	cursor.execute("SELECT * from `Inventory`.`tbl_location`;")
 	data = cursor.fetchall() #data from database
-	return render_template("locations.html", value=data)
+	return render_template("template.html", my_string="Location Table", value=data)
+
+@app.route('/js/<path:path>')
+def send_js(path): 
+	return send_from_directory('js', path)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("error.html")
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/javascript-file.js
@@ -0,0 +1,11 @@
+document.getElementById("Home").onclick = function () {
+    location.href = "http://127.0.0.1:5000/";
+};
+    
+document.getElementById("myProducts").onclick = function () {
+    location.href = "http://127.0.0.1:5000/Products";
+};
+    
+document.getElementById("myLocations").onclick = function () {
+    location.href = "http://127.0.0.1:5000/Locations";
+};
